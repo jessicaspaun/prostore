6ffc0e99f5d1cd4a56b4a0949195a6e8b01fdfbd
Jessica Desotell <jyspaun@mtu.edu>
2025-08-11 10:19:56 -0400

    Initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,16 @@
+import ProductList from "@/components/ui/shared/product/product-list";
+import { getLatestProducts } from "@/lib/actions/product.actions";
+
+
+const Homepage = async () => {
+  const latestProducts = await getLatestProducts();
+
+  return ( 
+    <div className='space-y-8'>
+      <h2 className='h2-bold'>Latest Products</h2>
+      <ProductList title='Newest Arrivals' data={latestProducts} />
+    </div>
+  );
+}
+ 
+export default Homepage;
